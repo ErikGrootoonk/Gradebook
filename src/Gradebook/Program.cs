79e51eb72a8f5e98c62a980bf99903f4b32961be
Grootoonk <erik.grootoonk@centric.eu>
2020-03-16 17:39:41 +0100

    looping

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -6,6 +6,17 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {   
+            var numbers = new [] {21.7, 34.6, 35.4 };
+
+            var result = 0.0;
+            foreach(double number in numbers)
+
+            {
+               result += number; 
+            }
+
+            Console.WriteLine(result);
+            
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello {args[0]} !");
