48ce2cfc9278d88eb37405ac52b10efaaf4e74a7
Erik Grootoonk <erik.grootoonk@centric.eu>
2021-02-10 16:53:03 +0100

    added unit test and a statistics class

--- src/Gradebook/Book.cs
+++ src/Gradebook/Book.cs
@@ -4,43 +4,45 @@ using System.Text;
 
 namespace Gradebook
 {
-    class Book
+    public class Book
     {
         public Book(string name)
         {
             grades = new List<double>();
             this.name = name;
         }
-        
+
         public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
 
-        public void ShowStatistics() {
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-            
-            foreach(var number in grades)
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            foreach (var grade in grades)
 
             {
-                lowGrade = Math.Min(number, lowGrade);
-                highGrade = Math.Max(number, highGrade);
-               result += number; 
+                result.Low = Math.Min(grade, result.Low);
+                result.High = Math.Max(grade, result.High);
+                result.Average += grade;
             }
 
-    result /= grades.Count;
+            result.Average /= grades.Count;
 
-            Console.WriteLine($"The lowest grade is {lowGrade}");
-            Console.WriteLine($"The highest grade is {highGrade}");
-            Console.WriteLine($"The average grade is {result:N1}");
-        
+            return result;
+
+
+
         }
 
         private List<double> grades;
         private string name;
 
-        
+
     }
 }
